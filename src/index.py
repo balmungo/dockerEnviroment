2a6bd5c99b45cca7ff7e9b32033b163a4a6c957f
Benjamin <behemonthe@gmail.com>
2025-06-08 19:45:22 -0400

    agregando archivo de verificacion y pruebas index

---
+++ src/index.py
@@ -0,0 +1,10 @@
+import torch
+print(torch.__version__)
+import torchaudio
+print(torchaudio.list_audio_backends())
+import sys
+print(sys.version)
+import json
+print(json.dumps({"a":1,"b":2}))
+from transformers import pipeline
+print(pipeline('sentiment-analysis')('hugging face is the best'))
